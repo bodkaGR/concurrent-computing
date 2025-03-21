f36596c832cd696bffdbbdb721dc65d23137b6db
Bohdan <grontkovskijboda@gmail.com>
2025-03-21 22:42:22 +0100

    [refactor] 4 matrix sizes added

--- Lab_3/src/main/java/net/bodkasoft/matrix/MatrixMultiplicationApplication.java
+++ Lab_3/src/main/java/net/bodkasoft/matrix/MatrixMultiplicationApplication.java
@@ -1,18 +1,33 @@
 package net.bodkasoft.matrix;
 
+import net.bodkasoft.matrix.executor.StripMatrixMultiplicationExecutor;
 import net.bodkasoft.matrix.utils.MatrixUtils;
+import net.bodkasoft.matrix.utils.Result;
 
 public class MatrixMultiplicationApplication {
 
-    private static final int THREADS_AMOUNT = 2;
+    private static final int THREADS_AMOUNT = 8;
     private static final int MATRIX_SIZE = 10;
 
     public static void main(String[] args) {
-        int[][] matrixA = MatrixUtils.generateMatrix(MATRIX_SIZE, MATRIX_SIZE);
-        int[][] matrixB = MatrixUtils.generateMatrix(MATRIX_SIZE, MATRIX_SIZE);
 
-        System.out.printf("Matrices size: %dx%d\n\n", matrixA.length, matrixA[0].length);
+        int[] matrixSizes = new int[]{ 504, 1008, 1512, 2016, 2520, 3024 };
 
-        MatrixMultiplication.run(matrixA, matrixB, THREADS_AMOUNT);
+        for (int i = 0; i < matrixSizes.length; i++) {
+            int[][] matrixA = MatrixUtils.generateMatrix(matrixSizes[i], matrixSizes[i]);
+            int[][] matrixB = MatrixUtils.generateMatrix(matrixSizes[i], matrixSizes[i]);
+
+            System.out.printf("Matrices size: %dx%d\n", matrixA.length, matrixA[0].length);
+
+            Result result = new Result(matrixA.length, matrixB[0].length);
+
+            long start = System.currentTimeMillis();
+            new StripMatrixMultiplicationExecutor(result, THREADS_AMOUNT).execute(matrixA, matrixB);
+            long end = System.currentTimeMillis();
+            System.out.println("Strip matrix multiplication took: " + (end - start) + "ms");
+            System.out.println();
+        }
+
+//        MatrixMultiplication.run(matrixA, matrixB, THREADS_AMOUNT);
     }
 }
