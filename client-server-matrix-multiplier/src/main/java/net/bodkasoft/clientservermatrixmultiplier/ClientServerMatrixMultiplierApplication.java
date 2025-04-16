119af53b4f40b04817dee20106fbc09c15d20fb9
Bohdan <grontkovskijboda@gmail.com>
2025-04-16 23:21:16 +0200

    lab-8 initial commit

---
+++ client-server-matrix-multiplier/src/main/java/net/bodkasoft/clientservermatrixmultiplier/ClientServerMatrixMultiplierApplication.java
@@ -0,0 +1,11 @@
+package net.bodkasoft.clientservermatrixmultiplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerMatrixMultiplierApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientServerMatrixMultiplierApplication.class, args);
+    }
+}
---
+++ client-server-matrix-multiplier/src/test/java/net/bodkasoft/clientservermatrixmultiplier/ClientServerMatrixMultiplierApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bodkasoft.clientservermatrixmultiplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServerMatrixMultiplierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
